a40c839861ce4a9a87173c4aedd521b7c7064b5f
Oliver Lomanat <lomanatoliver@gmail.com>
2025-06-17 09:16:05 +0300

    Updating the code

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+// function addingEventListener() {
+//   const button = document.getElementById('button');
+//   button.addEventListener('click', () => {
+//     console.log('Button clicked!');
+//   });
+// }
+
+
 function addingEventListener() {
-  const button = document.getElementById('button');
-  button.addEventListener('click', () => {
-    console.log('Button clicked!');
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
   });
 }
